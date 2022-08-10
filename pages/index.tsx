1815e55ee8ccaa6a4b2a1d6fe90e2d7d5e1942e1
dylst <cdylan127@gmail.com>
2022-08-09 21:56:58 -0700

    Implement SSR for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import { client, urlFor } from '../sanity';
+import { Post } from '../typings';
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+const Home: NextPage<Props> = ({ posts }: Props) => {
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
@@ -40,3 +46,23 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+    name,
+    image
+  },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await client.fetch(query);
+
+  return {
+    props: { posts },
+  };
+};
